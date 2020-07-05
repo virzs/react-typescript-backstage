448fe80c98ea87ba5aa5430812abfc3dcefa5c45
virzs <2369310227@qq.com>
2020-07-05 19:17:44 +0800

    增加react router

---
+++ src/data/main-data.ts
@@ -0,0 +1,22 @@
+export const authorData = {
+  author: "Vir",
+  introduction: "前端，主要掌握HTML5、CSS3、JavaScript、Vue。",
+};
+
+export const pageData = [
+  {
+    pageName: "个人简介",
+    pageValue: "first",
+    introduction: "这是第一页",
+  },
+  {
+    pageName: "职业技能",
+    pageValue: "second",
+    introduction: "这是第二页",
+  },
+  {
+    pageName: "兴趣爱好",
+    pageValue: "third",
+    introduction: "这是第三页",
+  },
+];
--- src/page/Index/Index.tsx
+++ src/page/Index/Index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Button } from "antd";
+import { authorData, pageData } from "../../data/main-data";
+
+let page = pageData.map((item, index) => {
+  return (
+    <div key={index}>
+      <p>{item.pageName}</p>
+      <p>{item.pageValue}</p>
+      <p>{item.introduction}</p>
+    </div>
+  );
+});
 
 const Index = () => {
   return (
     <div className="App-header">
-      <p>主页</p>
-      <Button type="primary">默认图标</Button>
-      <Button type="link" href="https://blog.virs.xyz">博客</Button>
+      <p>{authorData.author}</p>
+      <p>{authorData.introduction}</p>
+      <Button type="link" href="https://blog.virs.xyz">
+        博客
+      </Button>
+      <div>{page}</div>
     </div>
   );
 };
