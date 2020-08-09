8d07bc4f1528ae3271d57af3f4b34d9c84cd1102
virzs <2369310227@qq.com>
2020-08-09 17:55:25 +0800

    router导入更改

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.less";
-import Index from "./page/Index/Index";
 function App() {
   return (
     <div className="App">
-      <Index></Index>
     </div>
   );
 }
