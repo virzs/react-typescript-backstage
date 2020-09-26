a57426ce4c323325456ca09bb5818995854c0fc2
virzs <2369310227@qq.com>
2020-09-27 00:11:43 +0800

    星空背景beta1

---
+++ src/components/Star_Canvas/starCanvas.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+export interface starsField {
+  x: number;
+  y: number;
+  radius: number;
+  vx: number;
+  vy: number;
+}
+
+let starConfig = ["#fff", "#f17c67", "#DDF0ED", "#376956"];
+
+class StarCanvas extends React.Component {
+  canvas: React.RefObject<HTMLCanvasElement>;
+  constructor(props: any) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+  componentDidMount() {
+    this.tick();
+  }
+  drawing = () => {
+    const canvas = this.canvas.current;
+    if (canvas !== undefined && canvas !== null) {
+      let ctx = canvas.getContext("2d");
+      if (ctx !== null) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        let stars: Array<starsField> = [], //数组
+          FPS: number = 1, //每秒的帧数
+          x: number = canvas.width; //星星的个数
+
+        //创建星星
+        for (let i = 0; i < x; i++) {
+          stars.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: Math.random(),
+            vx: Math.floor(Math.random() * 10) - 5,
+            vy: Math.floor(Math.random() * 10) - 5,
+          });
+        }
+
+        //绘制场景
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.globalCompositeOperation = "lighter";
+        for (let i = 0, x = stars.length; i < x; i++) {
+          let s = stars[i];
+          ctx.fillStyle =
+            starConfig[Math.floor(Math.random() * starConfig.length)];
+          ctx.beginPath();
+          ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+          ctx.fill();
+        }
+        //更新星星位置
+        for (let i = 0, x = stars.length; i < x; i++) {
+          let s = stars[i];
+          s.x += s.vx / FPS;
+          s.y += s.vy / FPS;
+          if (s.x < 0 || s.x > canvas.width) s.x = -s.x;
+          if (s.y < 0 || s.y > canvas.height) s.y = -s.y;
+        }
+      }
+    }
+  };
+  tick = () => {
+    this.drawing();
+    requestAnimationFrame(this.drawing);
+  };
+  render() {
+    return <canvas ref={this.canvas}></canvas>;
+  }
+}
+
+export default StarCanvas;
--- src/views/Index/mainInfo.tsx
+++ src/views/Index/mainInfo.tsx
@@ -3,6 +3,8 @@ import "./mainInfo.scss";
 import { authorData } from "@/data/main-data";
 import SpotlightText from "@@/src/components/Spotlight_Text/SpotlightText";
 import { getBingImage } from "@@/src/api/bingImg/bing";
+import HeWeather from "@@/src/components/He_Weather/heWeather";
+import StarCanvas from "@/components/Star_Canvas/starCanvas";
 
 export interface stateTypes {
   bingImage: any;
@@ -40,17 +42,13 @@ class MainInfo extends React.Component<any, stateTypes> {
           height: `${window.innerHeight}px`,
         }}
       >
-        <div
-          className="bg"
-          style={{
-            backgroundImage: `url("http://cn.bing.com${
-              this.state.bingImage.images
-                ? this.state.bingImage.images[0].url
-                : ""
-            }")`,
-          }}
-        ></div>
+        <div className="bg">
+          <StarCanvas></StarCanvas>
+        </div>
         <div className="content">
+          <div className="flex-weather">
+            <HeWeather></HeWeather>
+          </div>
           <p className="author">{"我是" + authorData.author}</p>
           <SpotlightText size={58}>{authorData.introduction}</SpotlightText>
           <ul>{Links}</ul>
