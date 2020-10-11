70824fd8a73ff24b8a6ed2661cb11258dc597a29
virzs <2369310227@qq.com>
2020-10-11 22:34:35 +0800

    天气组件初版

--- src/components/He_Weather/heWeather.tsx
+++ src/components/He_Weather/heWeather.tsx
@@ -4,36 +4,59 @@ import { getCityInfo, getHeWeather } from "@/api/heWeather/heWeather";
 import "./hw.scss";
 
 export interface WeatherProps {
-  location: Array<object> | any;
-  weather: object | any;
+  location: Array<object> | any; //定位地点
+  weather: object | any; //当前位置天气
+  isLoading: Boolean; //是否正在加载
+  isError: Boolean; //是否出现错误
+  errMsg: string;
+  refresh: () => void; //刷新
 }
 
-function Icon(props: WeatherProps) {
-  if (props.location.length > 0 && Object.keys(props.weather).length > 0) {
-    let city = props.location[0].adm2; //当前城市
-    let temp = props.weather.temp; //当前温度
-    let text = props.weather.text; //当前温度描述
-    let icon = props.weather.icon; //当前温度图标
-    return (
-      <div className="weather-box">
-        <div className="weather-icon">
-          <img
-            src={require(`../../../public/static/heWeatherIcon/color-64/${icon}.png`)}
-            alt=""
-          />
+class Icon extends React.Component<WeatherProps> {
+  constructor(props: WeatherProps) {
+    super(props);
+  }
+  render() {
+    let isError = this.props.isError; //是否错误
+    let isLoading = this.props.isLoading; //是否正在加载
+    let ele: JSX.Element = <div></div>;
+    if (isError) {
+      //TODO手动刷新按钮
+      ele = (
+        <div className="weather-box" onClick={this.props.refresh}>
+          {this.props.errMsg || "加载天气错误，请刷新"}
         </div>
-        <span className="weather-temp">{temp}</span>
-        <div className="weather-unit">
-          <span>℃</span>
+      );
+    }
+    if (isLoading) {
+      //TODO加载动画
+      ele = <div className="weather-box">正在加载天气，请稍后</div>;
+    }
+    if (
+      this.props.location.length > 0 &&
+      Object.keys(this.props.weather).length > 0 &&
+      !this.props.isLoading
+    ) {
+      // let city = props.location[0].adm2; //当前城市
+      let temp = this.props.weather.temp; //当前温度
+      // let text = props.weather.text; //当前温度描述
+      let icon = this.props.weather.icon; //当前温度图标
+      ele = (
+        <div className="weather-box" onClick={this.props.refresh}>
+          <div className="weather-icon">
+            <img
+              src={require(`../../../public/static/heWeatherIcon/color-64/${icon}.png`)}
+              alt=""
+            />
+          </div>
+          <span className="weather-temp">{temp}</span>
+          <div className="weather-unit">
+            <span>℃</span>
+          </div>
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="weather-box">
-        <p className="weather-loading">加载中</p>
-      </div>
-    );
+      );
+    }
+    return ele;
   }
 }
 
@@ -43,12 +66,17 @@ class HeWeather extends React.Component<any, any> {
     this.state = {
       location: [],
       weather: {},
+      isLaoding: true,
+      isError: false,
+      errMsg: "",
     };
+    this.refresh = this.refresh.bind(this);
   }
   componentDidMount() {
     this.getLocation();
   }
   getLocation() {
+    this.setState({ isLoading: true });
     getGeolocation(
       (position) => {
         let latitude = position.coords.latitude.toFixed(2);
@@ -57,27 +85,54 @@ class HeWeather extends React.Component<any, any> {
         this.getLocalWeather(longitude, latitude);
       },
       (positionError) => {
-        console.error(positionError.message);
+        console.log(positionError);
+        if (positionError.code === 1) {
+          this.setState({ errMsg: "用户拒绝位置权限" });
+        }
+        this.setState({ isError: true });
+        this.setState({ isLoading: false });
       }
     );
   }
   getLocalCity(longitude: string, latitude: string) {
     let location = `${longitude},${latitude}`;
-    getCityInfo(location).then((res: any) => {
-      let data = res.data.location;
-      this.setState({ location: data });
-    });
+    getCityInfo(location)
+      .then((res: any) => {
+        let data = res.data.location;
+        this.setState({ location: data });
+        this.setState({ isLoading: false });
+      })
+      .catch((err) => {
+        this.setState({ isError: true });
+        this.setState({ isLoading: false });
+      });
   }
   getLocalWeather(longitude: string, latitude: string) {
     let location = `${longitude},${latitude}`;
-    getHeWeather(location).then((res: any) => {
-      let data = res.data.now;
-      this.setState({ weather: data });
-    });
+    getHeWeather(location)
+      .then((res: any) => {
+        let data = res.data.now;
+        this.setState({ weather: data });
+        this.setState({ isLoading: false });
+      })
+      .catch((err) => {
+        this.setState({ isError: true });
+        this.setState({ isLoading: false });
+      });
+  }
+  refresh() {
+    this.getLocation();
   }
   render() {
     return (
-      <Icon location={this.state.location} weather={this.state.weather}></Icon>
+      <Icon
+        location={this.state.location}
+        weather={this.state.weather}
+        isLoading={this.state.isLoading}
+        isError={this.state.isError}
+        errMsg={this.state.errMsg}
+        refresh={this.refresh}
+      ></Icon>
     );
   }
 }
