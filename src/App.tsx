988395bf2a3ece1664e41fd444b12e4825505735
virzs <2369310227@qq.com>
2020-12-01 23:51:12 +0800

    后台框架路由匹配规则

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,29 @@ import Index from "@/page/Index";
 import About from "@/page/About";
 import Error from "@/page/Error";
 import Login from "@/page/Auth/login";
+import Backstage from "@/page/Backstage";
 import "./App.less";
+import { backstageRoutes } from "./router/router";
+
+function Recursive(route: any[]) {
+  let list: any = [];
+  // 递归将路由tree转为普通列表
+  let handleTreeList = (treeList: any[], parentPath: string | null = null) => {
+    treeList.forEach((i) => {
+      i.path = parentPath ? `${parentPath}${i.path}` : i.path;
+      list.push(i);
+      if (i.children) {
+        handleTreeList(i.children, i.path);
+      }
+    });
+  };
+  handleTreeList(route);
+  return list.map((i: { path: any; component: any }) => {
+    return (
+      <Route path={i.path} key={i.path} component={i.component} exact></Route>
+    );
+  });
+}
 
 function App() {
   return (
@@ -14,6 +36,7 @@ function App() {
           <Route path="/about" component={About} exact></Route>
           <Route path="/" component={Index} exact></Route>
           <Route path="/auth/login" component={Login} exact></Route>
+          <Backstage>{Recursive(backstageRoutes)}</Backstage>
           {/* 错误页面 */}
           <Route component={Error}></Route>
         </Switch>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,41 @@
+import BackstageIndex from "@/views/Backstage/Index";
+import Backstage from "@/page/Backstage";
 import Index from "@/page/Index";
-const routes = [{
-  path: "/",
-  component: Index,
-  childRoutes: [
-    { path: "about", component: "" },
-    { path: "inbox", component: "" },
-  ],
-}];
-export default routes;
+import MdEditor from "@/components/Md_editor/MdEditor";
+const routes = [
+  {
+    path: "/",
+    component: Index,
+    childRoutes: [
+      { path: "about", component: "" },
+      { path: "inbox", component: "" },
+    ],
+  },
+];
+
+const backstageRoutes = [
+  {
+    name: "管理后台",
+    path: "/backstage",
+    component: Backstage,
+    meta: {},
+    children: [
+      {
+        name: "首页",
+        path: "/index",
+        component: BackstageIndex,
+        meta: {},
+        children: [
+          {
+            name: "编辑器",
+            path: "/editor",
+            component: MdEditor,
+            meta: {},
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export { routes, backstageRoutes };
