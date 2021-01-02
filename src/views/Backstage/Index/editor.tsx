6fbe9a51cc4d12a02c35c9c8aec74e578d4f843c
virzs <2369310227@qq.com>
2021-01-02 14:54:39 +0800

    编辑器后台页面

---
+++ src/views/Backstage/Index/editor.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MdEditor from "@/components/Md_editor/MdEditor";
+
+class editor extends React.Component<any, any> {
+  render() {
+    return <MdEditor />;
+  }
+}
+
+export default editor;
