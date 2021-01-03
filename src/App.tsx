cdfdf593b6b9faacab24c85fde66edfb40c48e5d
virzs <2369310227@qq.com>
2021-01-03 20:50:08 +0800

    调整路由方式

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.less";
 import VRouter from "./router/router";
 import store from "./store";
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="APP">
       <Provider store={store}>
-        <Router>
+        <BrowserRouter>
           <VRouter></VRouter>
-        </Router>
+        </BrowserRouter>
       </Provider>
     </div>
   );
