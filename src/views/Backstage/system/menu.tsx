35f5a4b1ef2641390dde7494394e8bdb759c31be
virzs <2369310227@qq.com>
2021-02-04 11:50:12 +0800

    调整菜单部分页面

--- src/views/Backstage/system/menu.tsx
+++ src/views/Backstage/system/menu.tsx
@@ -1,5 +1,5 @@
 import { treeList } from "@/api/system/menu";
-import { Button, Tree } from "antd";
+import { Button, Card, Tree } from "antd";
 import React from "react";
 import "./styles/menu.style.scss";
 
@@ -37,19 +37,38 @@ class Menu extends React.Component<any, any> {
   render() {
     return (
       <div className="system-menu-view">
-        <div className="handler-box">
-          <Button>添加</Button>
-          <Button>编辑</Button>
-          <Button>删除</Button>
-        </div>
         <div className="content-box">
-          <div className="menu-tree">
+          <Card
+            className="menu-tree"
+            title="菜单列表"
+            size="small"
+            extra={
+              <Button size="small" type="primary">
+                添加
+              </Button>
+            }
+          >
             <Tree
               titleRender={(nodeData: any) => {
                 return `${nodeData.name}`;
               }}
               treeData={this.state.menu}
             ></Tree>
+          </Card>
+          <div className="content-box">
+            <Card
+              className="menu-box"
+              title="菜单信息"
+              size="small"
+              extra={
+                <>
+                  <Button size="small">编辑</Button>
+                  <Button size="small" danger>
+                    删除
+                  </Button>
+                </>
+              }
+            ></Card>
           </div>
         </div>
       </div>
