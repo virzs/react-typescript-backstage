7569ddfead6d959a6b5baee39ad93540aeffb2f0
virzs <2369310227@qq.com>
2021-02-04 11:49:48 +0800

    菜单接口

--- src/api/system/menu.ts
+++ src/api/system/menu.ts
@@ -1,6 +1,6 @@
 import request from "@/utils/axios";
 
-export interface createType {
+interface createType {
   readonly name: string;
   readonly alias: string;
   readonly path: string;
@@ -11,6 +11,15 @@ export interface createType {
   readonly hidden: number;
 }
 
+interface updateType extends createType {
+  readonly id: string;
+}
+
+interface idType {
+  readonly id: string;
+}
+
+//新建菜单
 export const create = (data: createType) => {
   return request({
     url: "/api/system/menu/create",
@@ -19,6 +28,34 @@ export const create = (data: createType) => {
   });
 };
 
+//编辑菜单
+export const update = (data: updateType) => {
+  return request({
+    url: "/api/system/menu/update",
+    method: "put",
+    data,
+  });
+};
+
+//删除菜单
+export const del = (data: idType) => {
+  return request({
+    url: "/api/system/menu/delete",
+    method: "delete",
+    data,
+  });
+};
+
+//新建菜单
+export const detail = (data: idType) => {
+  return request({
+    url: "/api/system/menu/detail",
+    method: "get",
+    data,
+  });
+};
+
+//获取树形列表
 export const treeList = () => {
   return request({
     url: "/api/system/menu/tree-list",
