a6bdf9ecd3fe3f6ee9d216e4620792ef51f03e85
virzs <2369310227@qq.com>
2021-02-12 20:33:54 +0800

    增加菜单收缩，调整菜单样式

--- src/page/Backstage/components/menu.tsx
+++ src/page/Backstage/components/menu.tsx
@@ -1,16 +1,24 @@
 import { backstageRouterTree } from "@/data/backstage.router";
 import { deepCopy } from "@/utils/utils";
+import { MenuOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 import SubMenu from "antd/lib/menu/SubMenu";
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style/menu.style.scss";
 
-class VMemu extends React.Component<any, any> {
+interface VMenuPropTypes {
+  toggle: () => void;
+}
+
+class VMemu extends React.Component<VMenuPropTypes, any> {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
-  constructor(props: any) {
+  constructor(props: VMenuPropTypes) {
     super(props);
-    this.state = { collapsed: true };
   }
+  toggle = () => {
+    this.props.toggle();
+  };
   componentDidMount() {}
   handleRenderMenuItem(routes: any, parentPath: string | null = null) {
     let routesList = deepCopy(routes);
@@ -32,9 +40,14 @@ class VMemu extends React.Component<any, any> {
   }
   render() {
     return (
-      <Menu mode="inline" theme="dark" inlineCollapsed={this.state.collapsed}>
-        {this.handleRenderMenuItem(backstageRouterTree, "/backstage")}
-      </Menu>
+      <div className="v-menu">
+        <div className="toggle-menu" onClick={this.toggle}>
+          <MenuOutlined />
+        </div>
+        <Menu mode="inline">
+          {this.handleRenderMenuItem(backstageRouterTree, "/backstage")}
+        </Menu>
+      </div>
     );
   }
 }
