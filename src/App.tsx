652a5b1593f86affc0e436a0615d42d791dd2ad3
virzs <2369310227@qq.com>
2021-02-27 16:28:25 +0800

    配置redux持久化

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import "./App.less";
+import { GlobalLoading } from "./components/Global_Loading/GlobalLoading";
 import VRouter from "./router/router";
-import store from "./store";
+import store, { persistor } from "./store";
 
 function App() {
   return (
     <div className="APP">
       <Provider store={store}>
-        <BrowserRouter>
-          <VRouter></VRouter>
-        </BrowserRouter>
+        <PersistGate loading={<GlobalLoading />} persistor={persistor}>
+          <BrowserRouter>
+            <VRouter></VRouter>
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,30 @@
 import { combineReducers, createStore } from "redux";
 import { userLogin } from "./reducers/user.reducer";
+import { persistReducer, persistStore } from "redux-persist";
+import storageSession from "redux-persist/lib/storage";
 
 /**
  * Store文件
  */
 
+// redux数据持久化配置
+const reduxPersistConfig = {
+  key: "root",
+  storage: storageSession, //缓存方式
+  blacklist: [], // 黑名单
+};
+
+// 合并reducers
 const appReducers = combineReducers({ userLogin });
 
-const store = createStore(appReducers, {
-  userLogin: false,
-});
+// 将合并的reducers添加到持久化配置中
+// 将该函数验证设为any或具体类型可跳过persistStore函数的类型验证
+// persistReducer<any,any>(xxx,xxx)
+const appPersistReducers = persistReducer(reduxPersistConfig, appReducers);
+
+// 创建store
+const store = createStore(appPersistReducers);
+
+export const persistor = persistStore(store);
 
 export default store;
